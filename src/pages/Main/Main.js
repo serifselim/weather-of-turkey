1332e9da17bed429571baf9b86e896fd782c7406
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-08-29 14:18:34 +0300

    added react-bootstrap package

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,19 +1,22 @@
 import React from 'react';
+// Components
 import { Map, Show } from '../../components';
 import { Header } from '../../components';
+// Bootstrap
+import { Container } from 'react-bootstrap';
 
 const Main = () => {
     return (
         <>
             <Header />
-            <div className='container'>
+            <Container>
                 <div className='d-flex flex-column align-items-center justify-content-center mt-0 mt-md-5 text-light'>
                     <div className='turkey-map'>
                         <Map />
                     </div>
                     <Show />
                 </div>
-            </div>
+            </Container>
         </>
     );
 };
